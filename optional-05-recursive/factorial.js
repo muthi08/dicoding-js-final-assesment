e0a7c23aac02db4463d00b6ca41f08baba91acd7
muthi08 <muthinasyithoh@gmail.com>
2024-10-22 19:27:06 +0700

    05

--- optional-05-recursive/factorial.js
+++ optional-05-recursive/factorial.js
@@ -1,4 +1,10 @@
-function factorial(n) {}
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
 
 // Jangan hapus kode di bawah ini!
 export default factorial;
--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,4 +1,14 @@
-function fibonacci(n) {}
+function fibonacci(n) {
+    if (n === 0) {
+        return [0];
+    } else if (n === 1) {
+        return [0, 1];
+    } else {
+        const deretFibonacci = fibonacci(n - 1);
+        deretFibonacci.push(deretFibonacci[deretFibonacci.length - 1] + deretFibonacci[deretFibonacci.length - 2]);
+        return deretFibonacci;
+    }
+}
 
 // Jangan hapus kode di bawah ini!
 export default fibonacci;
